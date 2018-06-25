e5bc3f788f682d2bbcb4583d9bc74d38aa945c36
risk1996 <williamdariansutedjo@gmail.com>
2018-06-25 14:04:54 +0700

    Initial commit

---
+++ mp3s/.sh
@@ -0,0 +1,112 @@
+gtts-cli "nol" -l "id" -o 0.mp3
+gtts-cli "satu" -l "id" -o 1.mp3
+gtts-cli "dua" -l "id" -o 2.mp3
+gtts-cli "tiga" -l "id" -o 3.mp3
+gtts-cli "empat" -l "id" -o 4.mp3
+gtts-cli "lima" -l "id" -o 5.mp3
+gtts-cli "enam" -l "id" -o 6.mp3
+gtts-cli "tujuh" -l "id" -o 7.mp3
+gtts-cli "delapan" -l "id" -o 8.mp3
+gtts-cli "sembilan" -l "id" -o 9.mp3
+gtts-cli "sepuluh" -l "id" -o 10.mp3
+gtts-cli "sebelas" -l "id" -o 11.mp3
+gtts-cli "dua belas" -l "id" -o 12.mp3
+gtts-cli "tigas belas" -l "id" -o 13.mp3
+gtts-cli "empat belas" -l "id" -o 14.mp3
+gtts-cli "lima belas" -l "id" -o 15.mp3
+gtts-cli "enam belas" -l "id" -o 16.mp3
+gtts-cli "tujuh belas" -l "id" -o 17.mp3
+gtts-cli "delapan belas" -l "id" -o 18.mp3
+gtts-cli "sembilan belas" -l "id" -o 19.mp3
+gtts-cli "dua puluh" -l "id" -o 20.mp3
+gtts-cli "dua puluh satu" -l "id" -o 21.mp3
+gtts-cli "dua puluh dua" -l "id" -o 22.mp3
+gtts-cli "dua puluh tiga" -l "id" -o 23.mp3
+gtts-cli "dua puluh empat" -l "id" -o 24.mp3
+gtts-cli "dua puluh lima" -l "id" -o 25.mp3
+gtts-cli "dua puluh enam" -l "id" -o 26.mp3
+gtts-cli "dua puluh tujuh" -l "id" -o 27.mp3
+gtts-cli "dua puluh delapan" -l "id" -o 28.mp3
+gtts-cli "dua puluh sembilan" -l "id" -o 29.mp3
+gtts-cli "tiga puluh" -l "id" -o 30.mp3
+gtts-cli "tiga puluh satu" -l "id" -o 31.mp3
+gtts-cli "tiga puluh dua" -l "id" -o 32.mp3
+gtts-cli "tiga puluh tiga" -l "id" -o 33.mp3
+gtts-cli "tiga puluh empat" -l "id" -o 34.mp3
+gtts-cli "tiga puluh lima" -l "id" -o 35.mp3
+gtts-cli "tiga puluh enam" -l "id" -o 36.mp3
+gtts-cli "tiga puluh tujuh" -l "id" -o 37.mp3
+gtts-cli "tiga puluh delapan" -l "id" -o 38.mp3
+gtts-cli "tiga puluh sembilan" -l "id" -o 39.mp3
+gtts-cli "empat puluh" -l "id" -o 40.mp3
+gtts-cli "empat puluh satu" -l "id" -o 41.mp3
+gtts-cli "empat puluh dua" -l "id" -o 42.mp3
+gtts-cli "empat puluh tiga" -l "id" -o 43.mp3
+gtts-cli "empat puluh empat" -l "id" -o 44.mp3
+gtts-cli "empat puluh lima" -l "id" -o 45.mp3
+gtts-cli "empat puluh enam" -l "id" -o 46.mp3
+gtts-cli "empat puluh tujuh" -l "id" -o 47.mp3
+gtts-cli "empat puluh delapan" -l "id" -o 48.mp3
+gtts-cli "empat puluh sembilan" -l "id" -o 49.mp3
+gtts-cli "lima puluh" -l "id" -o 50.mp3
+gtts-cli "lima puluh satu" -l "id" -o 51.mp3
+gtts-cli "lima puluh dua" -l "id" -o 52.mp3
+gtts-cli "lima puluh tiga" -l "id" -o 53.mp3
+gtts-cli "lima puluh empat" -l "id" -o 54.mp3
+gtts-cli "lima puluh lima" -l "id" -o 55.mp3
+gtts-cli "lima puluh enam" -l "id" -o 56.mp3
+gtts-cli "lima puluh tujuh" -l "id" -o 57.mp3
+gtts-cli "lima puluh delapan" -l "id" -o 58.mp3
+gtts-cli "lima puluh sembilan" -l "id" -o 59.mp3
+gtts-cli "enam puluh" -l "id" -o 60.mp3
+gtts-cli "enam puluh satu" -l "id" -o 61.mp3
+gtts-cli "enam puluh dua" -l "id" -o 62.mp3
+gtts-cli "enam puluh tiga" -l "id" -o 63.mp3
+gtts-cli "enam puluh empat" -l "id" -o 64.mp3
+gtts-cli "enam puluh lima" -l "id" -o 65.mp3
+gtts-cli "enam puluh enam" -l "id" -o 66.mp3
+gtts-cli "enam puluh tujuh" -l "id" -o 67.mp3
+gtts-cli "enam puluh delapan" -l "id" -o 68.mp3
+gtts-cli "enam puluh sembilan" -l "id" -o 69.mp3
+gtts-cli "tujuh puluh" -l "id" -o 70.mp3
+gtts-cli "tujuh puluh satu" -l "id" -o 71.mp3
+gtts-cli "tujuh puluh dua" -l "id" -o 72.mp3
+gtts-cli "tujuh puluh tiga" -l "id" -o 73.mp3
+gtts-cli "tujuh puluh empat" -l "id" -o 74.mp3
+gtts-cli "tujuh puluh lima" -l "id" -o 75.mp3
+gtts-cli "tujuh puluh enam" -l "id" -o 76.mp3
+gtts-cli "tujuh puluh tujuh" -l "id" -o 77.mp3
+gtts-cli "tujuh puluh delapan" -l "id" -o 78.mp3
+gtts-cli "tujuh puluh sembilan" -l "id" -o 79.mp3
+gtts-cli "delapan puluh" -l "id" -o 80.mp3
+gtts-cli "delapan puluh satu" -l "id" -o 81.mp3
+gtts-cli "delapan puluh dua" -l "id" -o 82.mp3
+gtts-cli "delapan puluh tiga" -l "id" -o 83.mp3
+gtts-cli "delapan puluh empat" -l "id" -o 84.mp3
+gtts-cli "delapan puluh lima" -l "id" -o 85.mp3
+gtts-cli "delapan puluh enam" -l "id" -o 86.mp3
+gtts-cli "delapan puluh tujuh" -l "id" -o 87.mp3
+gtts-cli "delapan puluh delapan" -l "id" -o 88.mp3
+gtts-cli "delapan puluh sembilan" -l "id" -o 89.mp3
+gtts-cli "sembilan puluh" -l "id" -o 90.mp3
+gtts-cli "sembilan puluh satu" -l "id" -o 91.mp3
+gtts-cli "sembilan puluh dua" -l "id" -o 92.mp3
+gtts-cli "sembilan puluh tiga" -l "id" -o 93.mp3
+gtts-cli "sembilan puluh empat" -l "id" -o 94.mp3
+gtts-cli "sembilan puluh lima" -l "id" -o 95.mp3
+gtts-cli "sembilan puluh enam" -l "id" -o 96.mp3
+gtts-cli "sembilan puluh tujuh" -l "id" -o 97.mp3
+gtts-cli "sembilan puluh delapan" -l "id" -o 98.mp3
+gtts-cli "sembilan puluh sembilan" -l "id" -o 99.mp3
+gtts-cli "seratus" -l "id" -o 100.mp3
+gtts-cli "persen" -l "id" -o pct.mp3
+gtts-cli "hasil tes" -l "id" -o hsl.mp3
+gtts-cli "biuret" -l "id" -o bi.mp3
+gtts-cli "benedict" -l "id" -o be.mp3
+gtts-cli "tingkat akurasi" -l "id" -o ta.mp3
+gtts-cli "analisa selesai" -l "id" -o sls.mp3
+gtts-cli "positif" -l "id" -o pos.mp3
+gtts-cli "negatif" -l "id" -o neg.mp3
+gtts-cli "error" -l "id" -o err.mp3
+gtts-cli "tidak ditemukan" -l "id" -o td.mp3
+gtts-cli "kamera" -l "id" -o cam.mp3
---
+++ test.py
@@ -0,0 +1,158 @@
+# import cv2
+# import RPi
+# diff = cv2.imread("dif.jpg")
+# crop = diff[0:200, 0:100]
+# cv2.imshow("Cropped", crop)
+# cv2.waitKey(0)
+
+# import RPi.GPIO as GPIO
+# import time
+
+# GPIO.setmode(GPIO.BCM)
+
+# GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
+# GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
+
+# i=0
+
+# try:
+#     while True:
+#          button_state = GPIO.input(23)
+#          if button_state == False:
+#             GPIO.output(24, True)
+#             i += 1
+#             print('Button Pressed ' + str(i) + " times.")
+#             time.sleep(0.2)
+#          else:
+#             GPIO.output(24, False)
+# except:
+#     GPIO.cleanup()
+
+# import RPLCD
+# import RPi.GPIO as GPIO
+# import time
+# try:
+#     lcd = RPLCD.CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+#     lcd.write_string(u'Hello World')
+#     time.sleep(1.5)
+#     def lcd_write(row, col, str):
+#         lcd.cursor_pos = (row, col)
+#         lcd.write_string(str)
+#         return
+    
+#     lcd_write(0, 0, u'UrineAnalyzerV.1')
+#     lcd_write(1, 0, u'~~ Booting Up ~~')
+#     time.sleep(3)
+#     while True:
+#         lcd_write(1,0,"Clk: %s" %time.strftime("%H:%M:%S:%f"))
+#     GPIO.cleanup()
+# except:
+#     GPIO.cleanup()
+
+# import picamera
+# import time
+
+# CAMERA_SHARPNESS             = 0
+# CAMERA_CONTRAST              = 0
+# CAMERA_BRIGHTNESS            = 50
+# CAMERA_SATURATION            = 0
+# CAMERA_ISO                   = 400
+# CAMERA_EXPOSURE_COMPENSATION = 0
+# CAMERA_EXPOSURE_MODE         = 'auto'
+# CAMERA_METER_MODE            = 'average'
+# CAMERA_AWB_MODE              = 'auto'
+# CAMERA_AWB_GAINS             = (0,0)
+
+# # Camera Initialization
+# def camera_init():
+#     global camera
+#     camera = picamera.PiCamera()
+#     camera.resolution = (800, 600)
+#     camera.video_stabilization = False
+#     camera.image_effect = 'none'
+#     camera.color_effects = None
+#     camera.rotation = 0
+#     camera.hflip = False
+#     camera.vflip = False
+#     camera.crop = (0.0, 0.0, 1.0, 1.0)
+#     # picamera.PiRenderer(camera, fullscreen=False, window=(0,0,2592, 1944))
+#     camera.annotate_text = '|'
+#     return
+
+# # Camera load adjustments
+# def camera_reload():
+#     camera.sharpness = CAMERA_SHARPNESS
+#     camera.contrast = CAMERA_CONTRAST
+#     camera.brightness = CAMERA_BRIGHTNESS
+#     camera.saturation = CAMERA_SATURATION
+#     camera.ISO = CAMERA_ISO
+#     camera.exposure_compensation = CAMERA_EXPOSURE_COMPENSATION
+#     camera.exposure_mode = CAMERA_EXPOSURE_MODE
+#     camera.meter_mode = CAMERA_METER_MODE
+#     camera.awb_mode = CAMERA_AWB_MODE
+#     camera.awb_gains = CAMERA_AWB_GAINS
+
+# # Camera take picture
+# def take_picture(filename):
+#     camera_init()
+#     camera_reload()
+#     camera.start_preview()
+#     time.sleep(.25)
+#     camera.capture(filename)
+#     camera.stop_preview()
+#     camera.close()
+#     return
+
+# camera_init()
+# # camera_reload()
+# camera.led = False
+# camera.start_preview()
+# camera.led = False
+# time.sleep(5)
+# camera.capture("aft.jpg", use_video_port=True)
+# camera.stop_preview()
+# camera.close()
+
+# import cv2
+# import numpy
+# BEFORE_IMAGE_FILENAME        = "bef.jpg"
+# AFTER_IMAGE_FILENAME         = "aft.jpg"
+# DIFFERENCE_IMAGE_FILENAME    = "dif.jpg"
+# IMAGE_WIDTH                  = 800
+# IMAGE_HEIGHT                 = 600
+# bef = cv2.imread(BEFORE_IMAGE_FILENAME)
+# aft = cv2.imread(AFTER_IMAGE_FILENAME)
+# diff = cv2.absdiff(aft, bef)
+# diff = cv2.bitwise_not(diff)
+# cv2.imwrite(DIFFERENCE_IMAGE_FILENAME, diff)
+# diff_l = diff[0:IMAGE_HEIGHT, 0              :IMAGE_WIDTH/2]
+# diff_r = diff[0:IMAGE_HEIGHT, IMAGE_WIDTH/2+1:IMAGE_WIDTH  ]
+# diff_l_avg = numpy.round(numpy.average(numpy.average(diff_l, axis=0), axis=0))
+# diff_r_avg = numpy.round(numpy.average(numpy.average(diff_r, axis=0), axis=0))
+# print(diff_l_avg)
+# print(diff_r_avg)
+
+# import webcolors # https://stackoverflow.com/questions/9694165/convert-rgb-color-to-english-color-name-like-green-with-python
+
+# def closest_colour(requested_colour):
+#     min_colours = {}
+#     for key, name in webcolors.css3_hex_to_names.items():
+#         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+#         rd = (r_c - requested_colour[0]) ** 2
+#         gd = (g_c - requested_colour[1]) ** 2
+#         bd = (b_c - requested_colour[2]) ** 2
+#         min_colours[(rd + gd + bd)] = name
+#     return min_colours[min(min_colours.keys())]
+
+# def get_colour_name(requested_colour):
+#     try:
+#         closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+#     except ValueError:
+#         closest_name = closest_colour(requested_colour)
+#         actual_name = None
+#     return actual_name, closest_name
+
+# requested_colour = (151, 223, 172)
+# actual_name, closest_name = get_colour_name(requested_colour)
+
+# print "Actual colour name:", actual_name, ", closest colour name:", closest_name
